25edb90aa9049d58f4735f9aac49581657a5f2cb
0030481923007 <61607476+0030481923007@users.noreply.github.com>
2020-03-06 22:14:53 -0300

    Add files via upload

---
+++ Atividade1/PVolume/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace PVolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.raio = new System.Windows.Forms.TextBox();
+            this.altura = new System.Windows.Forms.TextBox();
+            this.volume = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.limpar = new System.Windows.Forms.Button();
+            this.sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Raio";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(78, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Altura";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(78, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Volume";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // raio
+            // 
+            this.raio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raio.Location = new System.Drawing.Point(187, 27);
+            this.raio.Name = "raio";
+            this.raio.Size = new System.Drawing.Size(100, 29);
+            this.raio.TabIndex = 3;
+            this.raio.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // altura
+            // 
+            this.altura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.altura.Location = new System.Drawing.Point(187, 100);
+            this.altura.Name = "altura";
+            this.altura.Size = new System.Drawing.Size(100, 29);
+            this.altura.TabIndex = 4;
+            this.altura.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // volume
+            // 
+            this.volume.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.volume.Location = new System.Drawing.Point(187, 178);
+            this.volume.Name = "volume";
+            this.volume.Size = new System.Drawing.Size(100, 29);
+            this.volume.TabIndex = 5;
+            this.volume.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // calcular
+            // 
+            this.calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcular.Location = new System.Drawing.Point(12, 264);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(94, 42);
+            this.calcular.TabIndex = 6;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // limpar
+            // 
+            this.limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.limpar.Location = new System.Drawing.Point(126, 264);
+            this.limpar.Name = "limpar";
+            this.limpar.Size = new System.Drawing.Size(90, 42);
+            this.limpar.TabIndex = 7;
+            this.limpar.Text = "Limpar";
+            this.limpar.UseVisualStyleBackColor = true;
+            this.limpar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // sair
+            // 
+            this.sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sair.Location = new System.Drawing.Point(234, 264);
+            this.sair.Name = "sair";
+            this.sair.Size = new System.Drawing.Size(100, 42);
+            this.sair.TabIndex = 8;
+            this.sair.Text = "Sair";
+            this.sair.UseVisualStyleBackColor = true;
+            this.sair.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 329);
+            this.Controls.Add(this.sair);
+            this.Controls.Add(this.limpar);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.volume);
+            this.Controls.Add(this.altura);
+            this.Controls.Add(this.raio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox raio;
+        private System.Windows.Forms.TextBox altura;
+        private System.Windows.Forms.TextBox volume;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Button limpar;
+        private System.Windows.Forms.Button sair;
+    }
+}
+
---
+++ Atividade1/PVolume/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PVolume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double verify1, verify2;
+
+            if ((double.TryParse(raio.Text, out verify1) &&
+                double.TryParse(altura.Text, out verify2)))
+            {
+                double calculo = Math.PI * Math.Pow(verify1,2)* verify2;
+                volume.Text = calculo.ToString("N2");
+            }
+            else
+                MessageBox.Show("Dados Inválidos!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            raio.Text = "";
+            altura.Clear();
+            volume.Text = String.Empty; //ou volume = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
