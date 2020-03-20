7f6d8f720c14b1be70383a8c720b23c0be0858ea
0030481923007 <61607476+0030481923007@users.noreply.github.com>
2020-03-20 18:43:05 -0300

    Add files via upload

---
+++ Atividade2/IMC.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace IMC
+{
+    partial class IMC
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Masculino = new System.Windows.Forms.RadioButton();
+            this.Altura = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Feminino = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Peso = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Masculino
+            // 
+            this.Masculino.AutoSize = true;
+            this.Masculino.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Masculino.Location = new System.Drawing.Point(76, 18);
+            this.Masculino.Name = "Masculino";
+            this.Masculino.Size = new System.Drawing.Size(81, 19);
+            this.Masculino.TabIndex = 0;
+            this.Masculino.TabStop = true;
+            this.Masculino.Text = "Masculino";
+            this.Masculino.UseVisualStyleBackColor = true;
+            this.Masculino.CheckedChanged += new System.EventHandler(this.Masculino_CheckedChanged);
+            // 
+            // Altura
+            // 
+            this.Altura.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Altura.Location = new System.Drawing.Point(105, 87);
+            this.Altura.Mask = "00.00";
+            this.Altura.Name = "Altura";
+            this.Altura.Size = new System.Drawing.Size(100, 21);
+            this.Altura.TabIndex = 1;
+            this.Altura.ValidatingType = typeof(int);
+            this.Altura.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.Altura_MaskInputRejected);
+            this.Altura.TextChanged += new System.EventHandler(this.Altura_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gênero:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Feminino);
+            this.groupBox1.Controls.Add(this.Masculino);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(29, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(153, 69);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Feminino
+            // 
+            this.Feminino.AutoSize = true;
+            this.Feminino.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Feminino.Location = new System.Drawing.Point(76, 41);
+            this.Feminino.Name = "Feminino";
+            this.Feminino.Size = new System.Drawing.Size(77, 19);
+            this.Feminino.TabIndex = 6;
+            this.Feminino.TabStop = true;
+            this.Feminino.Text = "Feminino";
+            this.Feminino.UseVisualStyleBackColor = true;
+            this.Feminino.CheckedChanged += new System.EventHandler(this.Feminino_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Altura";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Peso ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Peso
+            // 
+            this.Peso.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Peso.Location = new System.Drawing.Point(105, 129);
+            this.Peso.Mask = "00.00";
+            this.Peso.Name = "Peso";
+            this.Peso.Size = new System.Drawing.Size(100, 21);
+            this.Peso.TabIndex = 6;
+            this.Peso.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.Peso_MaskInputRejected);
+            this.Peso.TextChanged += new System.EventHandler(this.Peso_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(64, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 290);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Peso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Altura);
+            this.Name = "IMC";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.IMC_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton Masculino;
+        private System.Windows.Forms.MaskedTextBox Altura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton Feminino;
+        private System.Windows.Forms.MaskedTextBox Peso;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Atividade2/IMC.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class IMC : Form
+    {
+        public IMC()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double pesoIdeal, altura, pesoAtual;
+
+
+            if (Masculino.Checked)
+            {
+                
+                
+                
+                double.TryParse(Altura.Text, out altura);
+                double.TryParse(Peso.Text, out pesoAtual);
+                pesoIdeal = (72.7 * altura) - 58;
+                pesoIdeal = Math.Round(pesoIdeal, 1);
+
+
+                if (pesoAtual < pesoIdeal)
+                {
+                    MessageBox.Show("Abaixo do Peso Ideal");
+                }
+
+                else if (pesoAtual > pesoIdeal)
+                {
+                    MessageBox.Show("Acima do Peso Ideal");
+                }
+
+                else
+                {
+                    MessageBox.Show("Congartulações - Peso Ideal");
+                }
+            }
+
+            else if (Feminino.Checked)
+            {
+                double.TryParse(Altura.Text, out altura);
+                double.TryParse(Peso.Text, out pesoAtual);
+                pesoIdeal = (62.1 * altura) - 44.7;
+                pesoIdeal = Math.Round(pesoIdeal, 1);
+
+                if (pesoAtual < pesoIdeal)
+                {
+                    _ = MessageBox.Show("Abaixo do Peso Ideal");
+                }
+
+                else if (pesoAtual > pesoIdeal)
+                {
+                    MessageBox.Show("Acima do Peso Ideal");
+                }
+
+                else
+                {
+                    MessageBox.Show("Congartulações - Peso Ideal");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Selecione um Gênero");
+            }
+           
+            
+        }
+
+        private void Altura_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            
+        }
+
+        private void Peso_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+           
+        }
+
+        private void Altura_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void Peso_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void IMC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Feminino_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Masculino_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
