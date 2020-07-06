c52413f895aa805ac2653b86f2fefaebce46091b
0030481923007 <61607476+0030481923007@users.noreply.github.com>
2020-07-05 21:18:41 -0300

    Add files via upload

---
+++ PROJETO FINAL/PALUNO/PALUNO/Aluno.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PALUNO
+{
+    class Aluno
+    {
+        private int raaluno;
+        private string nomealuno;
+        private int cidadeidcidade;
+
+        public int Raaluno
+        {
+            get
+            {
+                return raaluno;
+            }
+            set
+            {
+                raaluno = value;
+            }
+        }
+
+
+        public string Nomealuno
+        {
+            get
+            {
+                return nomealuno;
+            }
+            set
+            {
+                nomealuno = value;
+            }
+        }
+
+
+
+        public int Cidadeidcidade
+        {
+            get
+            {
+                return cidadeidcidade;
+            }
+            set
+            {
+                cidadeidcidade = value;
+            }
+        }
+
+        public DataTable Listar()
+        {
+            SqlDataAdapter daAluno;
+
+            DataTable dtAluno = new DataTable();
+
+            try
+            {
+                daAluno = new SqlDataAdapter("SELECT * FROM TBALUNO", frmPrincipal.conexao);
+                daAluno.Fill(dtAluno);
+                daAluno.FillSchema(dtAluno, SchemaType.Source);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dtAluno;
+        }
+
+        public int Salvar()
+        {
+            int retorno = 0;
+
+
+            try
+            {
+                SqlCommand mycommand;
+                int nReg;
+
+                mycommand = new SqlCommand("INSERT INTO TBALUNO VALUES (@RA_ALUNO,@NOME_ALUNO,@CIDADE_ID_CIDADE)", frmPrincipal.conexao);
+
+                mycommand.Parameters.Add(new SqlParameter("@RA_ALUNO", SqlDbType.Int)); ;
+                mycommand.Parameters.Add(new SqlParameter("@NOME_ALUNO", SqlDbType.VarChar));
+                mycommand.Parameters.Add(new SqlParameter("@CIDADE_ID_CIDADE", SqlDbType.Int));
+
+                mycommand.Parameters["@RA_ALUNO"].Value = raaluno;
+                mycommand.Parameters["@NOME_ALUNO"].Value = nomealuno;
+                mycommand.Parameters["@CIDADE_ID_CIDADE"].Value = cidadeidcidade;
+
+                nReg = mycommand.ExecuteNonQuery();
+
+                if (nReg > 0)
+                {
+                    retorno = nReg;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return retorno;
+        }
+
+        public int Alterar()
+        {
+            int retorno = 0;
+
+
+            try
+            {
+                SqlCommand mycommand;
+                int nReg = 0;
+                mycommand = new SqlCommand("UPDATE TBALUNO SET NOME_ALUNO = @NOME_ALUNO ,CIDADE_ID_CIDADE = @CIDADE_ID_CIDADE WHERE RA_ALUNO = @RA_ALUNO", frmPrincipal.conexao);
+
+                mycommand.Parameters.Add(new SqlParameter("@RA_ALUNO", SqlDbType.Int));
+                mycommand.Parameters.Add(new SqlParameter("@NOME_ALUNO", SqlDbType.VarChar));
+                mycommand.Parameters.Add(new SqlParameter("@CIDADE_ID_CIDADE", SqlDbType.Int));
+
+                mycommand.Parameters["@RA_ALUNO"].Value = raaluno;
+                mycommand.Parameters["@NOME_ALUNO"].Value = nomealuno;
+                mycommand.Parameters["@CIDADE_ID_CIDADE"].Value = cidadeidcidade;
+
+                nReg = mycommand.ExecuteNonQuery();
+                if (nReg > 0)
+                {
+                    retorno = nReg;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return retorno;
+        }
+
+
+        public int Excluir()
+        {
+            int nReg = 0;
+
+            try
+            {
+                SqlCommand mycommand;
+
+                mycommand = new SqlCommand("DELETE FROM TBALUNO WHERE ra_aluno=@RA_ALUNO", frmPrincipal.conexao);
+                mycommand.Parameters.Add(new SqlParameter("@RA_ALUNO", SqlDbType.Int));
+                mycommand.Parameters["@RA_ALUNO"].Value = Convert.ToInt32(raaluno);
+
+                nReg = mycommand.ExecuteNonQuery();
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return nReg;
+        }
+
+
+    }
+}
---
+++ PROJETO FINAL/PALUNO/PALUNO/frmSobre.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace PALUNO
+{
+    partial class frmSobre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSobre));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(36, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(549, 144);
+            this.panel1.TabIndex = 4;
+            this.panel1.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(205, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Projeto Final";
+            this.label2.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(36, 350);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(549, 100);
+            this.panel4.TabIndex = 5;
+            this.panel4.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(27, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(495, 40);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Sorocaba 5 de Julho de 2020";
+            this.label7.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(36, 144);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(549, 206);
+            this.panel5.TabIndex = 6;
+            this.panel5.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(495, 40);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Matheus Martins de Oliveira\r\n0030481923007\r\n\r\n";
+            this.label3.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(585, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(38, 450);
+            this.panel3.TabIndex = 3;
+            this.panel3.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(36, 450);
+            this.panel2.TabIndex = 2;
+            this.panel2.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            // 
+            // frmSobre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSobre";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sobre";
+            this.Deactivate += new System.EventHandler(this.frmSobre_Deactivate);
+            this.Click += new System.EventHandler(this.frmSobre_Deactivate);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmSobre_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmSobre_KeyPress);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
